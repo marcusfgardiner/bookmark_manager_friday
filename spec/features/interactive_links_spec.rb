097b0bed62df10a1ace2fbe8f1a567b68fa32d5b
Serena Hathi <hathi.serena@gmail.com>
2018-02-16 15:14:11 +0000

    View link titles add turn link titles into URLs

--- spec/features/interactive_links_spec.rb
+++ spec/features/interactive_links_spec.rb
@@ -1,6 +1,16 @@
 feature 'Title has link in it that can be clicked' do
   scenario 'Viewing all titles on homepage' do
     visit '/'
-    expect(page).to have_link 'http://www.makersacademy.com'
+    expect(page).to have_selector(:link, "Makers Academy")
   end
+
+  scenario "Clicking on a title opens link in a new tab" do
+    visit '/'
+    click_link("Makers Academy")
+    expect(page).to have_current_path("http://www.makersacademy.com")
+  end
+
 end
+
+
+
